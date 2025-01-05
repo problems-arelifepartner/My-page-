6227e13e35eaeb14c047f00bde98dda6d33c1a14
MR|D3V1L <88584834+problems-arelifepartner@users.noreply.github.com>
2025-01-05 20:17:10 +0530

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,8 +1,12 @@
 <?php
+// Clear cookies and session
+setcookie("username", "", time() - 3600, "/");
+setcookie("logged_in", "", time() - 3600, "/");
+
 session_start();
 session_unset();
 session_destroy();
+
 header("Location: login.html");
 exit();
 ?>
-
